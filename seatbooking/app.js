412ff524b769d1139467f8c05106d7a627ea6d61
shashankrk46 <shashankrk46@gmail.com>
2020-04-25 19:10:38 +0530

    added functionality

---
+++ seatbooking/app.js
@@ -0,0 +1,34 @@
+const container=document.querySelector('.container');
+const seats=document.querySelectorAll('.row .seat:not(.occupied)');
+//queryselectorall gives node list
+const count=document.getElementById('count');
+const total=document.getElementById('total');
+const movieSelect=document.getElementById('movie');
+
+let ticketPrice=+movieSelect.value;  //+ coverts into numbers
+
+// update total and count
+function updateSelectedCount(){
+ const selectedSeats=document.querySelectorAll('.row .seat.selected')
+ 
+ const selectedSeatsCount=selectedSeats.length;
+ count.innerText=selectedSeatsCount;
+ total.innerText=selectedSeatsCount*ticketPrice;
+}
+
+// movie select evnt
+movieSelect.addEventListener('change',(e)=>{
+    ticketPrice=+e.target.value;
+    updateSelectedCount();
+
+})
+
+container.addEventListener('click',(e)=>{
+    if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
+       e.target.classList.toggle('selected');
+
+       updateSelectedCount();
+    }
+})
+
+
